70632f10e22dbcf097cfa3f27c8e1dba64663fc4
Ayush Yadav <aayush.sang@gmail.com>
2021-05-23 20:53:36 +0530

    Add Navbar Component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import logo from './logo.svg';
+import React from 'react'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import Navbar from './components/layout/Navbar'
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <Router>
+      <Navbar />
+    </Router>
   );
 }
 
