16adde42e92126d4222b0094e68e789ae6719f57
Ayush Yadav <aayush.sang@gmail.com>
2021-05-25 16:35:35 +0530

    refactor: Add check for invalid note ids

--- src/components/notes/NoteDetail.js
+++ src/components/notes/NoteDetail.js
@@ -7,19 +7,40 @@ const NoteDetail = (props) => {
     const id = props.match.params.id
     useFirestoreConnect([{ collection: 'notes', doc: id }])
     const note = useSelector(({ firestore: { data } }) => data.notes && data.notes[id])
-    return (
+    const noteMarkup = !isLoaded(note) ? (
         <div className="container section">
             <div className="card z-depth-0">
                 <div className="card-content">
-                    <span className="card title">{note?.title}</span>
-                    <p>{note?.content}</p>
+                    <span className="card title">Loading...</span>
                 </div>
                 <div className="card-action grey lighten-4 grey-text">
-                    <div>{moment(note?.createdAt.toDate()).calendar()}</div>
+                </div>
+            </div>
+        </div>
+    ) : isEmpty(note) ? (
+        <div className="container section">
+            <div className="card z-depth-0">
+                <div className="card-content">
+                    <span className="card title">The Note Content is Empty.</span>
+                </div>
+                <div className="card-action grey lighten-4 grey-text">
+                </div>
+            </div>
+        </div>
+    ) : (
+        <div className="container section">
+            <div className="card z-depth-0">
+                <div className="card-content">
+                    <span className="card title">{note.title}</span>
+                    <p>{note.content}</p>
+                </div>
+                <div className="card-action grey lighten-4 grey-text">
+                    <div>{moment(note.createdAt.toDate()).calendar()}</div>
                 </div>
             </div>
         </div>
     )
+    return noteMarkup
 }
 
 export default NoteDetail
